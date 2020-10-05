23d51743fa44125db8c409d09e04bf9f618b0293
Young Yu <youngyu2002@gmail.com>
2020-10-04 21:28:37 -0700

    added javadoc

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,14 +1,17 @@
 import processing.core.PApplet;
 
 public class BubbleSort extends PApplet {
-    private int numValues;
     private int[] values;
     private int i;
     private int j;
     private int rectWidth;
 
+    /**
+     * Defines initial environment properties such as screen size and to load media such as images and fonts as the
+     * program starts
+     */
     public void setup() {
-        numValues = 100;
+        int numValues = 100;
         size(800, 500);
         frameRate(1000);
 
@@ -22,6 +25,10 @@ public class BubbleSort extends PApplet {
         }
     }
 
+    /**
+     * Continuously executes the lines of code contained inside its block until the program is stopped or noLoop()
+     * is called
+     */
     public void draw() {
         background(0);
 
@@ -49,6 +56,13 @@ public class BubbleSort extends PApplet {
         }
     }
 
+    /**
+     * Swaps values at indices i and j in the given array.
+     *
+     * @param values array of values to be sorted
+     * @param i      index i
+     * @param j      index j
+     */
     private void swap(int[] values, int i, int j) {
         int temp = values[i];
         values[i] = values[j];
